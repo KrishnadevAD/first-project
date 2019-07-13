d21db7ecfacd2aff00f91c47ba286f99f2f4f225
Krishnadev Adhikari <50297033+KrishnadevAD@users.noreply.github.com>
2019-07-13 10:34:01 +0545

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,8 @@
+	<nav>
+		<a href="index.html">HOME</a>
+		<a href="inquery.html">INQUIRY</a>
+		<a href="offer.html">OFFER</a>
+		<a href="about.html">ABOUT</a>
+		<input type="text" placeholder="Search...">
+	
+</nav>
